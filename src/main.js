12950f4fafab3ddc1ac2f56acad41374f8ee3a0a
Ademola Taiwo <taiwoademola465@gmaile.com>
2024-05-24 07:31:19 +0100

    mage the header and hero responsive

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import "../style/modern-normalize.css";
 import "../style/style.css";
 
 // components
+import "../style/components/header.css";
+import "../style/components/hero.css";
+import "../style/components/features.css";
 
 // utility
 import "../style/utility.css";
