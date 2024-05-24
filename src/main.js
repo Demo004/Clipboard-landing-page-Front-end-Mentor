f09be8a4c9e6dabac14eefbce76a254d055fb29a
Ademola Taiwo <taiwoademola465@gmaile.com>
2024-05-24 18:00:35 +0100

    added logos and end section

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import "../style/style.css";
 import "../style/components/header.css";
 import "../style/components/hero.css";
 import "../style/components/features.css";
+import "../style/components/access.css";
+import "../style/components/logos.css";
+import "../style/components/end.css";
 
 // utility
 import "../style/utility.css";
